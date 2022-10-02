f13d718cfb3f0d37d0303342f63d2d211e336ce3
Unquiet Sugar <kirilask@gmail.com>
2022-10-02 11:27:44 +0300

    storage bug fix

--- src/hooks/useSetItems.ts
+++ src/hooks/useSetItems.ts
@@ -10,28 +10,29 @@ const useSetItems = () => {
 	const isLoaded = useRef<boolean>(false); // needed because of react 18 strict mode
 
 	useEffect(() => {
-		if (state.list.length > 0) {
-			setList(state.list);
-		}
-		if (state.historyList.length > 0) {
-			setHistory(state.historyList);
-		}
-	}, [state.list, state.historyList]);
-
-	useEffect(() => {
-		if (state.list.length === 0 && !isLoaded.current) {
-			const list = getList();
-			const historyList = getHistory();
-			console.log(historyList);
-			if (list) {
-				addList(list);
+		if (!isLoaded.current) {
+			if (state.list.length === 0 || state.historyList.length === 0) {
+				const list = getList();
+				const historyList = getHistory();
+				console.log(historyList);
+				if (list) {
+					addList(list);
+				}
+				if (historyList) {
+					addHistoryList(historyList);
+				}
+				isLoaded.current = true;
 			}
-			if (historyList) {
-				addHistoryList(historyList);
-			}
-			isLoaded.current = true;
 		}
 	}, []);
+
+	useEffect(() => {
+		setList(state.list);
+	}, [state.list]);
+
+	useEffect(() => {
+		setHistory(state.historyList);
+	}, [state.historyList]);
 };
 
 export default useSetItems;
